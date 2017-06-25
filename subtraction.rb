cbbbb68e9167d249cbb0b9f689afa2d1d1276c3c
bolyardk22 <collinsk22@mycu.concord.edu>
2017-06-25 19:02:06 -0400

    program which subtracts two numbers

---
+++ subtraction.rb
@@ -0,0 +1,20 @@
+puts "This program will subtract two numbers"
+
+def first_number
+	puts "What is the first number?"
+	first_number = gets.to_i
+end
+
+def second_number
+	puts "What is the second number?"
+	second_number = gets.to_i
+end
+
+def calculation
+	num1 = first_number
+	num2 = second_number
+	calculation = num1 - num2
+	p "#{num1} - #{num2} = #{calculation}"
+end
+
+calculation
